aee26c5754e91cf93096514d16d65f334268e1a2
Magnum2222 <magnumsapf@gmail.com>
2021-10-15 21:30:45 -0300

    Create Questão 02_Facebook.java

---
+++ ATIVIDADE2/Questão 02_Facebook.java
@@ -0,0 +1,13 @@
+package ATIVIDADE2;
+public class Questao02_Facebook{
+    public static void maind(String[]args){
+        Questao02_Facebook X = New Questao02_Facebook("Aprendendo a fazer Feijoada, mas, sem feijão e sem carne?",
+        X.curtidaspost();
+        X.curtidaspost();
+        X.curtidaspost();
+        System.out.println("Quantas curtidas esse post recebeu: "+X.getNumeroCurtidas());
+        X.compartilharPost();
+        X.compartilharPost();
+        System.out.println("Quantas vezes esse post foi compartilhado: ")
+    }
+}
